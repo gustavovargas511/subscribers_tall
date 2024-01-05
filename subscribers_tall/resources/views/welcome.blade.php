182cdcb3dde710756c9d2debf7085abe87200ae9
gustavovargas511 <gustavovargas511@gmail.com>
2024-01-04 21:21:18 -0600

    Layouts edition

--- subscribers_tall/resources/views/welcome.blade.php
+++ subscribers_tall/resources/views/welcome.blade.php
@@ -1,3 +1,20 @@
 <x-guest-layout>
-    
+    <div class="flex flex-col bg-rose-600 w-full h-screen">
+        <nav class="flex pt-5 justify-between container mx-auto text-pink-100">
+            <a class="text-3xl" href="/">
+                <x-application-logo class="w-16 h-16 fill-white">
+                </x-application-logo>
+            </a>
+            <div class="flex justify-end">
+                @auth
+                    <a href="{{ route('dashboard') }}">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                @endauth
+            </div>
+        </nav>
+    </div>
+    <div class="flex flex-col bg-pink-400 w-full h-screen">
+
+    </div>
 </x-guest-layout>
